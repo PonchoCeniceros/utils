a1046287c134957a37cb264051040055d0c72fe6
Giovanny Chavez <giovannychavez@MacBook-Pro-de-Gio.local>
2026-01-22 08:26:51 -0600

    [main] agregando funciones de utilidad basicas

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,4 @@
 /**
  * Función básica de sleep en ms
  */
-export function sleep(ms: number) {
-  new Promise((resolve) => setTimeout(resolve, ms));
-}
+export const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
